0b793be5ce063c0431279ac3a6a62ab2705db7f0
Andrew Belunov <cyberpunk.person@gmail.com>
2022-03-19 02:56:40 +0300

    Initial commit

---
+++ src/main/java/com/github/cyberpunkperson/retrayer/Application.java
@@ -0,0 +1,20 @@
+package com.github.cyberpunkperson.retrayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication(exclude = {
+		WebSocketServletAutoConfiguration.class,
+		ProjectInfoAutoConfiguration.class
+})
+@ConfigurationPropertiesScan
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
